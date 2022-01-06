9e5ccf61f2031df789989f36a81c45478c549407
andjamba <aurelien.ndjamba@gmail.com>
2022-01-06 16:18:09 +0100

    Rating.java: TODO: Map columns in data table RATING with corresponding java fields -> OK

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
@@ -1,12 +1,23 @@
 package com.nnk.springboot.domain;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import java.sql.Timestamp;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name = "rating")
 public class Rating {
-    // TODO: Map columns in data table RATING with corresponding java fields
+	// TODO: Map columns in data table RATING with corresponding java fields -> OK
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Integer id;
+	String moodysRating;
+	String sandPRating;
+	String fitchRating;
+	Integer orderNumber;
 }
