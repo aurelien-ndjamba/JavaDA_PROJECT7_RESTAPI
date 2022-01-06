45b8ead5f448af4b865f5b7a066d44833136d621
andjamba <aurelien.ndjamba@gmail.com>
2022-01-06 15:09:55 +0100

    CurvePoint.java: - TODO: Map columns in data table CURVEPOINT with corresponding java fields -> OK

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -1,15 +1,28 @@
 package com.nnk.springboot.domain;
 
-import org.hibernate.validator.constraints.Length;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name = "curvepoint")
 public class CurvePoint {
     // TODO: Map columns in data table CURVEPOINT with corresponding java fields
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Integer id;
+	@NotNull(message = "must not be null")
+	Integer curveId;
+	Timestamp asOfDate;
+	Double term;
+	Double value;
+	Timestamp creationDate;
 }
